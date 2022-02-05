c1f69769c6eca0ed5bf40f0a0c598b979b2f53c6
lorenzo <lorenzovitale89@gmail.com>
2022-02-05 15:40:19 +0100

    changed names

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="tinyman-py-sdk",
+    name="tinyman-py-sdk-old",
     description="Tinyman Python SDK",
     author="Tinyman",
     author_email="hello@tinyman.org",
@@ -14,11 +14,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     license="MIT",
     project_urls={
-        "Source": "https://github.com/tinyman/tinyman-py-sdk",
+        "Source": "https://github.com/lrnz-vtl/tinyman-py-sdk-old",
     },
     install_requires=["py-algorand-sdk >= 1.6.0"],
     packages=setuptools.find_packages(),
     python_requires=">=3.7",
-    package_data={'tinyman.v1': ['asc.json']},
+    package_data={'tinyman-old.v1': ['asc.json']},
     include_package_data=True,
 )
